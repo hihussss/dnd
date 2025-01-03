42f20a1ad3f2cd0c8585c0b00826973eec58c5ba
hihuss <hihusssss@gmail.com>
2025-01-03 13:24:32 +0300

    work on mistake2

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,18 @@
 import { Field } from "../class/Field";
-import { Storage } from "../class/Storage";
+//import { Storage } from "../class/Storage";
 import { DnD } from "../class/drag";
-// import { DnDMouses } from "../class/Dmouse";
+
 
 const container = document.querySelector(".container");
 
-// создаем колонки с разметкой и формой, сразу все колонки
+
 const field = new Field(container);
 field.createField();
 
 // работа с localStorage
-const storage = new Storage();
+//const storage = new Storage();
 
-// const dnd = new DnDMouses(container);
-// dnd.start();
+
   
 const dnd = new DnD(container);
 dnd.start()
